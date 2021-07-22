4aa1c9460a1a072c149858fe06dc8d10a6ec41b0
Tom <tmartinezrivero@alumno.unlam.edu.ar>
2021-07-22 17:48:40 -0300

    Implementar post con carga de archivo csv
    Se implementa función en el controlador que hace un post al servidor y verifica que se cargue un archivo .csv

--- back/rutasPelicula.js
+++ back/rutasPelicula.js
@@ -3,9 +3,56 @@ const express = require('express');
 const route = express.Router();
 const multer = require('multer');
 const path = require('path');
+const { send } = require('process');
 
 //importar controlador
 const controller = require('./controllers/peliculaController.js');
+
+//directorio donde se van a guardar los csv y nombre de los archivos
+const storage = multer.diskStorage({
+    destination: "./public/uploads/",
+    filename: function (req, file, cb) {
+        cb(
+            null,
+            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+        );
+    },
+});
+//subir archivo
+const upload = multer({
+    storage: storage,
+    fileFilter: function(req, file, cb){
+        
+        //verificar que sea .csv
+
+        if(file.mimetype == "text/csv"){
+            //formato correcto
+            return cb(null, true);
+        } else{ 
+            //formato incorrecto
+            return cb(null, false);
+        }
+    }
+});
+
+route
+//cargar libro desde csv
+    .post('/', upload.single("lista"), async(req, res) => {
+
+        try {
+            //verificar que se cargó un archivo
+            if(!req.file){
+                //no se subió ningun archivo o el formato no es correcto
+                throw ("no se subió archivo");
+            }
+            //se subió un archivo del formato correcto
+            console.log(req.file);
+            res.send("ok").status(200)
+        } catch (error) {
+            console.log(error);
+            res.send(error).status(400);
+        }
+    })
 
 
 
