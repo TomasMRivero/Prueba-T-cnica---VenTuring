d881a27c57f7c9d2eb92d476a2d56d6c2b000c39
Tom <tmartinezrivero@alumno.unlam.edu.ar>
2021-07-25 20:13:43 -0300

    crear middleware de autorizacion
    se crea middleware para las autorizaciones y se exceptuan las rutas de login y registro

--- back/app.js
+++ back/app.js
@@ -1,10 +1,14 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const unless = require('express-unless');
+const jwt = require('jsonwebtoken');
 
 //rutas
 const rutasPelicula = require('./rutasPelicula');
 const rutasAuth = require('./rutasAuth')
+
+const {buscarToken} = require('./services/authService')
 
 const app = express();
 
@@ -13,6 +17,41 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
+//middleware para autorizacion
+const auth = async(req, res, next) => {
+    try {
+        //verifica que el usuario este logueado
+        let token = req.headers['authorization'];
+        if(!token){
+            throw("no estas logueado");
+        }
+
+        token = token.replace('Bearer ', '');
+
+        //verifica que el token sea válido
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, usuario) => {
+            if (err) {
+                throw ("token invalido")
+            } else {
+                //envía los datos del usuario al request
+                req.usuario = usuario;
+                next();
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.send(error).status(400);        
+    }
+};
+
+auth.unless = unless;
+
+app.use(auth.unless({
+    path:[
+        {url:'/api/auth/login', methods:['POST']},
+        {url:'/api/auth/registro', methods:['POST']}
+    ]
+}));
 
 app.use('/api/pelicula', rutasPelicula);
 app.use('/api/auth', rutasAuth);
