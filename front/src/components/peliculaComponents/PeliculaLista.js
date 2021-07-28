d12f9e1d21853f619572a42c8210d6a612d0189f
Tom <tmartinezrivero@alumno.unlam.edu.ar>
2021-07-28 12:14:49 -0300

    Crear paginacion
    Se crean componentes para elegir cuantos resultados se muestran por página y navegar por las distintas páginas

--- front/src/components/peliculaComponents/PeliculaLista.js
+++ front/src/components/peliculaComponents/PeliculaLista.js
@@ -1,8 +1,8 @@
-import { Grid } from "@material-ui/core";
+import { Button, Grid, IconButton, InputLabel, MenuItem, Select, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios"
 import QueryString from "qs";
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { batch, useDispatch, useSelector } from "react-redux"
 import { getPeliculaIDs, getPeliculas } from "../../redux/actions";
 import BuscarPelicula from "./BuscarPelicula";
@@ -43,17 +43,26 @@ export default function PeliculaLista({location}){
     const peliculas = useSelector(state => peliculaIDs.map(id => state.peliculas[id]));
 
     async function fetch(){
-        await axios.get(`api/pelicula?limit=${limit + 1}&pagina=${pagina}`)
+        async function getLista(){
+            return await axios.get(`api/pelicula?limit=${limit}&pagina=${pagina}`)
+        }
+        async function getSiguientePagina(){
+            return await axios.get(`api/pelicula?limit=${limit}&pagina=${pagina+1}`)
+        }
+        Promise.all([getLista(), getSiguientePagina()])
             .then(response => {
-                const lista = response.data.slice(0, limit);
+                console.log(response)
+                const lista = response[0].data.slice(0, limit);
                 
                 batch(() => {
                     dispatch(getPeliculas(lista));
                     dispatch(getPeliculaIDs(lista.map(i => i.id).reverse()));
                 })
 
-                if (!response.data[limit]){
+                if (response[1].data.length === 0){
                     setUltimaPagina(true);
+                }else{
+                    setUltimaPagina(false);
                 }
                 setCargado(true)
 
@@ -65,9 +74,24 @@ export default function PeliculaLista({location}){
 
     useEffect(() => {
         fetch()
-    }, [dispatch])
+    }, [dispatch, limit, pagina])
 
     console.log(peliculas);
+
+    const onChangeLimit = useCallback((e) => {
+        e.preventDefault()
+        setLimit(e.target.value)
+    })
+
+    const nextPage = useCallback((e) => {
+        e.preventDefault();
+        setPagina(pagina + 1);
+    });
+    
+    const prevPage = useCallback((e) => {
+        e.preventDefault();
+        setPagina(pagina - 1);
+    });
 
     return(
         <div className={classes.root}>
@@ -77,8 +101,28 @@ export default function PeliculaLista({location}){
                 </Grid>
                 <Grid item xs={12}>
                         {cargado && peliculas.map(p =>
-                            <PeliculaItem pelicula={p} />
+                            <PeliculaItem key={p.id} pelicula={p} />
                         )}
+                </Grid>
+                <Grid item xs={6}>
+                    Mostrar resultados:
+                    <Select
+                        value={limit}
+                        onChange = {onChangeLimit}
+                        className= {classes.Select}
+                    >
+                        <MenuItem value={5}>5</MenuItem>
+                        <MenuItem value={10}>10</MenuItem>
+                        <MenuItem value={15}>15</MenuItem>
+                        <MenuItem value={20}>20</MenuItem>
+                        <MenuItem value={25}>25</MenuItem>
+                        <MenuItem value={30}>30</MenuItem>
+                    </Select>
+                </Grid>
+                <Grid item xs={6} style={{display: "flex", justifyContent:"center" ,alignItems:"center"}}>
+                    {pagina!=1 && <Button onClick={prevPage}>{"< " + (pagina - 1)}</Button>}
+                    <Typography style={{margin: 5}}>{"- " + pagina + " -"}</Typography>
+                    {!ultimaPagina && <Button onClick={nextPage}>{(pagina + 1) + " >"}</Button>}
                 </Grid>
             </Grid>
         </div>
