917b0f0882391242362381bf48d4924c6b1dbd82
Tom <tmartinezrivero@alumno.unlam.edu.ar>
2021-07-27 22:08:41 -0300

    crear redirecciones si el usuario está logueado
    se crean redirecciones en caso de que el usuario esté logueado para que no pueda volver a iniciar sesión con otra sesión abierta

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,38 @@
 import './App.css';
 
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import LoginScreen from './components/authComponents/LoginScreen';
 import RegistroScreen from './components/authComponents/RegistroScreen';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { autenticar } from './redux/actions/authActions';
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  const autenticado = useSelector(state => state.autenticado);
+  const autorizacion = axios.defaults.headers.common.authorization;
+
+  //verifica que haya un token en el localStorage y
+  //cambia el estado de autenticado a true para después hacer las redirecciones
+  useEffect(() => {
+    if(autorizacion !== null){
+      dispatch(autenticar());
+      console.log(autenticado);
+    }
+  }, [dispatch]);
+  
   return (
     <div className="App">
       <Switch>
-        <Route path="/login" component={LoginScreen} />
-        <Route path="/registro" component={RegistroScreen} />
+        <Route path="/login">
+          {!autenticado?<LoginScreen/>:<Redirect to="/"/>}
+        </Route>
+        <Route path="/registro">
+          {!autenticado?<RegistroScreen/>:<Redirect to="/"/>}
+        </Route>
       </Switch>
     </div>
   );
