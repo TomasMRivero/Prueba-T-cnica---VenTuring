1ed109b07dbdbad80ca9f0e72042f4a4d5ab1893
Tom <tmartinezrivero@alumno.unlam.edu.ar>
2021-07-28 13:23:50 -0300

    Crear logica del formulario
    se crea la función que llama a la api, postea la película nueva y la muestra al cargarse

--- front/src/components/peliculaComponents/CargarPeliculaForm.js
+++ front/src/components/peliculaComponents/CargarPeliculaForm.js
@@ -1,5 +1,11 @@
 import { Button, Grid, Link, TextField, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { Redirect } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { getPelicula } from "../../redux/actions";
+import PeliculaItem from "./PeliculaItem";
 
 //estilos
 const useStyles = makeStyles((theme) => ({
@@ -40,9 +46,53 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 export default function CargarPeliculaForm(){
+    const dispatch = useDispatch();
     const classes = useStyles();
+
+    const [titulo, setTitulo] = useState('');
+    const onChangeTitulo = useCallback((e) => {
+        setTitulo(e.target.value);
+    });
+    const [descripcion, setDescripcion] = useState('');
+    const onChangeDescripcion = useCallback((e) => {
+        setDescripcion(e.target.value);
+    });
+    const [anio, setAnio] = useState('');
+    const onChangeAnio = useCallback((e) => {
+        setAnio(e.target.value)
+    })
+
+    const [cargado, setCargado] = useState(false)
+
+    const pelicula = useSelector(state => state.pelicula);
+    const autenticado = useSelector(state => state.autenticado);
+
+    async function cargarPelicula(){
+        await axios.post('api/pelicula/alta', {
+            titulo,
+            descripcion,
+            anio
+        }).then(response => {
+            dispatch(getPelicula(response.data))
+        }).catch(error => {
+            console.error(error.response.data)
+        });
+    }
+
+    const onSubmit = useCallback((e) => {
+        e.preventDefault();
+        cargarPelicula();
+    }, [titulo, descripcion, anio]);
+    console.log(pelicula);
+
+    useEffect(() => {
+        setCargado(true);
+    })
+
     return(
-        <form className={classes.root} label="libro">
+        <form className={classes.root} label="libro" onSubmit={onSubmit}>
+            {!autenticado && cargado && <Redirect to="/login"/>}
+
             <Grid className={classes.container} container spacing={5}>
             
             <Grid item xs={12}>
@@ -51,6 +101,8 @@ export default function CargarPeliculaForm(){
                     label="Título"
                     variant="outlined"
                     required
+                    value = {titulo}
+                    onChange = {onChangeTitulo}
                 />
             </Grid>
 
@@ -60,6 +112,8 @@ export default function CargarPeliculaForm(){
                     label="Año"
                     variant="outlined"
                     required
+                    value = {anio}
+                    onChange = {onChangeAnio}
                 />
             </Grid>
 
@@ -70,6 +124,8 @@ export default function CargarPeliculaForm(){
                     variant="outlined"
                     required
                     multiline
+                    value = {descripcion}
+                    onChange = {onChangeDescripcion}
                 />
             </Grid>
 
@@ -80,6 +136,17 @@ export default function CargarPeliculaForm(){
             <Grid item xs={12}>
                 <Typography className={classes.newAcc} variant="h6"><Link >Cargar desde archivo</Link></Typography>
             </Grid>
+            
+            {pelicula.id &&
+            <Grid item container xs={12} spacing={0}>
+                <Grid item xs={12}>
+                    <Typography variant='h6'>Cargado:</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <PeliculaItem pelicula={pelicula}/>
+                </Grid>
+            </Grid>
+            }
 
         </Grid> 
         </form>
