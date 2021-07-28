9f43fd1cb283e88f3b1dc7c41a6704d2ac3ad718
Tom <tmartinezrivero@alumno.unlam.edu.ar>
2021-07-27 22:44:58 -0300

    Crear lógica del componente
    Se agregan las funciones para los botones de home y login

--- front/src/components/Nav.js
+++ front/src/components/Nav.js
@@ -3,13 +3,13 @@ import HomeIcon from '@material-ui/icons/Home';
 import { makeStyles } from "@material-ui/core/styles"
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router";
+import { useCallback } from "react";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
     botonMenu: {
-        marginRight: theme.spacing(2),
         color: '#0d2b45',
     },
     titulo: {
@@ -29,19 +29,51 @@ export default function Nav(){
     const history = useHistory();
     const autenticado = useSelector(state => state.autenticado);
 
+    const onClickHome = useCallback((e) => {
+        e.preventDefault();
+        history.push("/");
+    });
+
+    const onClickLogin = useCallback((e) => {
+        e.preventDefault();
+        history.push("/login");
+    });
+
+    const onClickLogout = useCallback((e) => {
+        e.preventDefault();
+    });
+
     return(
         <div className={classes.root}>
         
-            <AppBar position="static">
+            <AppBar position="static" style={{background:"#ffaa5e"}}>
                 <Toolbar>
-                    <IconButton edge="start" className={classes.botonMenu} color="inherit" aria-label="menu">
+                    <IconButton
+                        edge="start"
+                        className={classes.botonMenu}
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={onClickHome}
+                    >
                         <HomeIcon/>
                     </IconButton>
                     <Typography variant="h6" className={classes.titulo}>
                         Películas
                     </Typography>
-                    <Button edge="end" className={classes.boton} color="inherit">Iniciar sesión</Button>
-                    <Button edge="end" className={classes.boton} color="inherit">Cerrar sesión</Button>
+                    {!autenticado &&
+                    <Button
+                        edge="end"
+                        className={classes.boton}
+                        color="inherit"
+                        onClick={onClickLogin}
+                    >Iniciar sesión</Button>}
+                    {autenticado &&
+                    <Button
+                        edge="end"
+                        className={classes.boton}
+                        color="inherit"
+                        onClick={onClickLogout}
+                    >Cerrar sesión</Button>}
                 </Toolbar>
             </AppBar>
         
